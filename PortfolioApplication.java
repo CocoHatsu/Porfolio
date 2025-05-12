7d888aa518ded4954fd73a490753205cbf318c02
Ngozahmo <95936687+CocoHatsu@users.noreply.github.com>
2025-05-13 01:19:12 +0530

    Add files via upload

---
+++ PortfolioApplication.java
@@ -0,0 +1,15 @@
+package My.First.portfolio.example.Portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class PortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioApplication.class, args);
+	}
+
+
+}
